2c8d85a976b2c418886878b54dbeee4a45c02ab1
ahmed1amen <ahmed1amen.g@gmail.com>
2021-05-03 05:48:59 +0200

    Implement OK Http

--- app/src/main/java/com/example/keylesscarcontrol/Carkeys.java
+++ app/src/main/java/com/example/keylesscarcontrol/Carkeys.java
@@ -2,23 +2,32 @@ package com.example.keylesscarcontrol;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import org.jetbrains.annotations.NotNull;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.List;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -26,47 +35,126 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+
 public class Carkeys extends AppCompatActivity {
-    ImageView iacon,iacoff,ilock,iunlock,iwindon,iwindoff;
-    WebView web1 ;
+    ImageView iacon, iacoff, ilock, iunlock, iwindon, iwindoff;
+    WebView web1;
+
+
+    void callHttp(String url) {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        this.getUnsafeOkHttpClient().newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(getApplicationContext(), "Error ! ", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String myResponse = response.body().string();
+                    Carkeys.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "Done", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    private static OkHttpClient getUnsafeOkHttpClient() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain,
+                                                       String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain,
+                                                       String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[0];
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            return new OkHttpClient.Builder()
+                    .sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0])
+                    .hostnameVerifier(new HostnameVerifier() {
+                        @Override
+                        public boolean verify(String hostname, SSLSession session) {
+                            return true;
+                        }
+                    }).build();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carkeys);
 
-        iacon=(ImageView)findViewById(R.id.ivacon);
-        iacoff=(ImageView)findViewById(R.id.ivacoff);
-        ilock=(ImageView)findViewById(R.id.ivlock);
-        iunlock=(ImageView)findViewById(R.id.ivunlock);
-        iwindoff=(ImageView)findViewById(R.id.ivwindoff);
-        iwindon=(ImageView)findViewById(R.id.ivwindon);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        iacon = (ImageView) findViewById(R.id.ivacon);
+        iacoff = (ImageView) findViewById(R.id.ivacoff);
+        ilock = (ImageView) findViewById(R.id.ivlock);
+        iunlock = (ImageView) findViewById(R.id.ivunlock);
+        iwindoff = (ImageView) findViewById(R.id.ivwindoff);
+        iwindon = (ImageView) findViewById(R.id.ivwindon);
+
 
         iacon.setOnClickListener(new View.OnClickListener() {
-            OkHttpClient client = new OkHttpClient();
 
-            void run(String url) throws IOException {
-                Request request = new Request.Builder()
-                        .url(url)
-                        .build();
-
-                try (Response response = client.newCall(request).execute()) {
-
-                }
-            }
 
             @Override
             public void onClick(View v) {
-                try {
-                    this.run("https://207.154.220.61:9443/I-2WVJmzCOtGoGi3uRZ0585eDTqD2w73/update/D13?value=1");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+
+                String url = "https://207.154.220.61:9443/I-2WVJmzCOtGoGi3uRZ0585eDTqD2w73/update/D13?value=1";
+                callHttp(url);
 
 
             }
         });
+
+
+        iacoff.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                String url = "https://207.154.220.61:9443/I-2WVJmzCOtGoGi3uRZ0585eDTqD2w73/update/D13?value=0";
+                callHttp(url);
+
+
+            }
+        });
+
         //Initialize And Assign Variables
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
@@ -77,19 +165,19 @@ public class Carkeys extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.keys:
 
                         return true;
                     case R.id.home:
                         startActivity(new Intent(getApplicationContext()
-                                ,HomeActivity.class));
-                        overridePendingTransition(0,0);
+                                , HomeActivity.class));
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.gps:
                         startActivity(new Intent(getApplicationContext()
-                                ,GPSActivity.class));
-                        overridePendingTransition(0,0);
+                                , GPSActivity.class));
+                        overridePendingTransition(0, 0);
                         return true;
                 }
                 return false;
@@ -98,4 +186,14 @@ public class Carkeys extends AppCompatActivity {
 
 
     }
+
+
 }
+
+
+
+
+
+
+
+
